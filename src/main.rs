a91541996bfec879cbba62bb104bf913f84eab92
Nelson Chen <crazysim@gmail.com>
2017-10-15 02:05:15 -0700

    format

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,34 @@
-#[macro_use]
-extern crate rouille;
+extern crate bus;
 extern crate nix;
 extern crate nix_ptsname_r_shim;
-extern crate multiqueue;
+#[macro_use]
+extern crate rouille;
+use bus::{Bus, BusReader};
 
 use std::{thread, time};
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 
 use rouille::websocket;
 use rouille::Response;
 
 fn main() {
-    let (send, recv) = multiqueue::broadcast_queue::<u8>(2);
+    let bus_mutex = Arc::new(Mutex::new(Bus::new(5)));
+
+    let loop_bus_mutex = bus_mutex.clone();
     thread::spawn(move || {
+        let mut counter = 0;
         loop {
-            thread::sleep(time::Duration::from_secs(1));
-            match send.try_send(8u8) {
-                _ => {}
-            }
+            thread::sleep(time::Duration::from_millis(100));
+            let mut bus = loop_bus_mutex.lock().unwrap();
+            println!("Broadcasting {}", counter);
+            bus.broadcast(counter);
+            counter += 1;
         }
     });
-    let recv_mutex = Mutex::new(recv);
 
+    let server_bus_mutex = bus_mutex.clone();
     println!("Now listening on 127.0.0.1:8000");
     rouille::start_server("127.0.0.1:8000", move |request| {
-        let recv = recv_mutex.lock().unwrap().clone();
-        for _ in recv.try_iter() {}
-        let recv_inner = recv.add_stream();
-        // Drain
-
         router!(request,
             (GET) (/) => {
                 Response::empty_404()
@@ -36,9 +36,11 @@ fn main() {
 
             (GET) (/ws) => {
                 let (response, websocket) = try_or_400!(websocket::start(&request, None::<&str>));
+                let request_mutex = server_bus_mutex.clone();
+                let bus_reader = request_mutex.lock().unwrap().add_rx();
                 thread::spawn(move || {
                     let ws = websocket.recv().unwrap();
-                    websocket_handling_thread(ws, recv_inner);
+                    websocket_handling_thread(ws, bus_reader);
                 });
                 response
             },
@@ -47,18 +49,18 @@ fn main() {
     });
 }
 
-fn websocket_handling_thread(mut websocket: websocket::Websocket, recv: multiqueue::BroadcastReceiver<u8>) {
+fn websocket_handling_thread(mut websocket: websocket::Websocket, mut recv: BusReader<u8>) {
     loop {
-        let data = recv.recv().unwrap();
-        let res = websocket.send_text(&format!("{:?}", data));
-        match res {
-            Ok(()) => {}
-            Err(_) => {
-                println!("oh no!");
-                recv.unsubscribe();
-                break
+        let data = match recv.recv() {
+            Ok(d) => d,
+            _ => break,
+        };
+        match websocket.send_text(&format!("{:?}", data)) {
+            Ok(_) => {}
+            _ => {
+                println!("Got dropped");
+                break;
             }
-        }
+        };
     }
-
 }
