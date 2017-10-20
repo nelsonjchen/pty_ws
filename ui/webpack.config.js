859f2635184138144e7adcf27f339c1b7334a9d9
Nelson Chen <crazysim@gmail.com>
2017-10-19 17:59:12 -0700

    Setup HTML page injection

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,6 +1,7 @@
 
 var path = require('path')
 var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
@@ -55,7 +56,14 @@ module.exports = {
   performance: {
     hints: false
   },
-  devtool: '#eval-source-map'
+  devtool: '#eval-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true,
+    }),
+  ],
 }
 
 if (process.env.NODE_ENV === 'production') {
